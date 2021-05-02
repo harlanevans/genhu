509a26ce6e2330dabc177ec483805afc6ff1c0b6
harlan evans <harlanevans5@gmail.com>
2021-05-02 14:08:17 -0600

    add carousel new images, fix react-reveal fade animation to be more simple, which was affecting text-overflow

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Fade } from "react-reveal";
+import { Fade, Zoom } from "react-reveal";
 import Carousel from "react-bootstrap/Carousel";
 import "./Landing.scss";
 
 import LandingKids from "../../assets/images/HeroCarousel/KidsLanding.jpg";
 import UrosWomen from "../../assets/images/HeroCarousel/Uros-women.jpeg";
-import PeruBoys from "../../assets/images/HeroCarousel/PeruBoys.png";
+import StarvingBoy from "../../assets/images/HeroCarousel/StarvingBoy.jpg";
+import MenSeeds from "../../assets/images/HeroCarousel/MenSeeds.jpg";
+import Refugees from "../../assets/images/HeroCarousel/Refugees.jpg";
+import WomenConference from "../../assets/images/HeroCarousel/Womensconference.jpg";
 
 import ProvenPage from "./Proven/Proven";
 import Success from "./Success/Success";
@@ -17,8 +20,8 @@ const Landing = () => {
   return (
     <>
       <Fade>
-        <section id='landing-page'>
-        <HeroPage />
+        <section id="landing-page">
+          <HeroPage />
         </section>
         <ProvenPage />
         <Success />
@@ -37,14 +40,19 @@ const HeroPage = () => (
     <Carousel.Item>
       <div id="image-container">
         <Carousel.Caption id="carousel-text">
-          <Fade left cascade duration={2000}>
-            <h1 id="carousel-text-details">Generations</h1>
-            <h1 id="carousel-text-details">Humanitarian</h1>
-            <p></p>
+          <Fade duration={2000}>
+            <h1 id="carousel-larger-text">Generations</h1>
+            <h1 id="carousel-larger-text">Humanitarian</h1>
+            <br />
+            <p id="carousel-smaller-text">
+              Transforming street children toward an abundance of health, hope,
+              and loving homes— thereby breaking the cycle of scarcity for
+              generations to come.{" "}
+            </p>
           </Fade>
         </Carousel.Caption>
         <img
-          className=" kenburns-bottom-right"
+          className="kenburns-bottom-right"
           alt="Espinal Happy and Smiling Kids"
           src={LandingKids}
           id="kids-landing-image"
@@ -54,14 +62,77 @@ const HeroPage = () => (
     <Carousel.Item>
       <div id="image-container">
         <Carousel.Caption id="carousel-text">
-          <Fade left cascade duration={1500}>
-            <h1 id="carousel-text-details">You have cared</h1>
-            <p></p>
+          <Fade duration={2000}>
+            <h1 id="carousel-larger-text">La Guajira, Colombia</h1>
+            {/* <h1 id="carousel-larger-text">Nurturing</h1> */}
+            <br />
+            <p id="carousel-smaller-text">
+              Some 70 children starved to death last year in this region. We are
+              now feeding a whole school and digging a well to provide local
+              water.{" "}
+            </p>
           </Fade>
         </Carousel.Caption>
         <img
           className=" kenburns-bottom-right"
-          alt="Espinal Happy and Smiling Kids"
+          alt="Starving boy"
+          src={StarvingBoy}
+          id="kids-landing-image"
+        />
+      </div>
+    </Carousel.Item>
+    <Carousel.Item>
+      <div id="image-container">
+        <Carousel.Caption id="carousel-text">
+          <Fade duration={2000}>
+            <h1 id="carousel-larger-text">
+              Women's Conference in Ibague, Colombia
+            </h1>
+            <p id="carousel-smaller-text">
+              More than 600 women attended. Women who are ex-guerrilla soldiers,
+              in the witness protection program, and internally displaced.{" "}
+            </p>
+          </Fade>
+        </Carousel.Caption>
+        <img
+          className=" kenburns-bottom-right"
+          alt="womens conference in Colombia"
+          src={WomenConference}
+          id="kids-landing-image"
+        />
+      </div>
+    </Carousel.Item>
+    <Carousel.Item>
+      <div id="image-container">
+        <Carousel.Caption id="carousel-text">
+          <Fade duration={2000}>
+            <h1 id="carousel-larger-text">Venezuelan Refugees</h1>
+            <p id="carousel-smaller-text">
+              Little girls just across the border into Cucuta, Colombia, with no
+              place to go. We feed 430 refugee children.{" "}
+            </p>
+          </Fade>
+        </Carousel.Caption>
+        <img
+          className=" kenburns-bottom-right"
+          alt="little girl refugees"
+          src={Refugees}
+          id="kids-landing-image"
+        />
+      </div>
+    </Carousel.Item>
+    <Carousel.Item>
+      <div id="image-container">
+        <Carousel.Caption id="carousel-text">
+          <Fade duration={2000}>
+            <h1 id="carousel-larger-text">
+              Join us on a humanitarian expedition to Peru in 2022
+            </h1>
+          </Fade>
+        </Carousel.Caption>
+        <img
+          className=" kenburns-bottom-right"
+          alt="uros women - join an expedition"
           src={UrosWomen}
           id="kids-landing-image"
         />
@@ -70,15 +141,16 @@ const HeroPage = () => (
     <Carousel.Item>
       <div id="image-container">
         <Carousel.Caption id="carousel-text">
-          <Fade left cascade duration={1500}>
-            <h1 id="carousel-text-details">Individual</h1>
-            <h1 id="carousel-text-details">Nurturing</h1>
+          <Fade duration={2000}>
+            <h1 id="carousel-larger-text">
+              Self-sustaining greenhouses for our orphanages
+            </h1>
           </Fade>
         </Carousel.Caption>
         <img
           className=" kenburns-bottom-right"
-          alt="Espinal Happy and Smiling Kids"
-          src={PeruBoys}
+          alt="men holding seeds for greenhouse"
+          src={MenSeeds}
           id="kids-landing-image"
         />
       </div>
